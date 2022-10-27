359a72d0a5d82836fa5cbc36b652a3bd2f5939cd
Samira Kaline <samirakaline123@gmail.com>
2022-10-27 18:48:36 -0300

    Adicionando useState e bootstrap

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,38 +1,31 @@
 import Food from "./Food";
+import { useRef, useState } from "react";
+import data from '../models/foods';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
-  const foods = [{
-    id : 0,
-    name : "Hambúrguer",
-    image : "imgs/hamburguer.jpg"
-  },
-  {
-    id : 1,
-    name : "Suco",
-    image : "imgs/suco.jpg"
-  },
-  {
-    id : 2,
-    name : "Café",
-    image : "imgs/café.jpg"
-  },
-  {
-    id : 3,
-    name : "Sanduíche",
-    image : "imgs/sanduiche.jpg"
-  },
-  {
-    id : 4,
-    name : "Café Mocca",
-    image : "imgs/cafemocca.jpg"
-  },
+  const [foods, setFoods] = useState(data);
+  const buttonAdd = useRef(null);
 
-];
+  const handleClick = () =>{
+    const food ={
+      id : 4,
+      name : "Café Mocca",
+      image : "imgs/cafemocca.jpg"
+    };
+
+    setFoods([...foods,food]);
+
+    buttonAdd.current.disabled = true;
+  }
 
   return (
     <div className="container">
       <h1>Menu</h1>
-      <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">+</button>
+      <button type="button" className=" btn btn-secondary round-circle mr-4 font-weight-bold" onClick={handleClick}
+      ref={buttonAdd}>+</button>
 
       <section className="card-deck my-3">
         {foods.map((food) => (
---
+++ src/models/foods.js
@@ -0,0 +1,24 @@
+const foods = [
+  {
+    id: 0,
+    name: 'Hambúrguer',
+    image: 'imgs/hamburguer.jpg',
+  },
+  {
+    id: 1,
+    name: 'Suco',
+    image: 'imgs/suco.jpg',
+  },
+  {
+    id: 2,
+    name: 'Café',
+    image: 'imgs/café.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sanduíche',
+    image: 'imgs/sanduiche.jpg',
+  },
+];
+
+export default foods;
