4fdc5044d3391190abff59dd8dc35964b4af897c
Samira <36956493+Samira-Kaline@users.noreply.github.com>
2022-12-21 17:04:27 -0300

    Revert "Butão de Delete e update"

--- src/componentes/DeleteCliente.jsx
+++ src/componentes/DeleteCliente.jsx
@@ -1,25 +1,12 @@
 import React,{useState} from 'react';
-import {Button,Form,Modal} from 'react-bootstrap';
-import {TbTrash} from 'react-icons/tb';
+import {Button,Modal} from 'react-bootstrap';
+import TableClients from './TableClients';
 
 const DeleteClient = () =>{
-  const [id,setId] = useState(0);
-  const [show,setShow] = useState();;
-
-  const handleClose = () =>{
-    handleClean();
-    setShow(false);
-  }
-
-  const handleShow = () =>{
-    setShow(true);
-  }
-  const handleClean = () =>{
-    setId(0);
-  }
+  const [id,setId] = useState();
 
   const handleDelete = () =>{
-    fetch(`http://localhost:3001/clientes/${id}`, { method: 'DELETE' })
+    fetch(`http://localhost:3001/clientes?q=${id}`, { method: 'DELETE' })
       .then((response) => {
         return response.json();
       })
@@ -27,44 +14,17 @@ const DeleteClient = () =>{
         console.log('Resposta com insucesso!');
       })
       .finally();
-
-      handleClean();
-      handleClose();
   }
 
   return(
     <div>
-      <Button 
-        variant='outline-danger '
-        className='float-end mx-1'
-        onClick={handleShow}
-      >
-        <TbTrash size={20}/>
-      </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>Deletar Cliente</Modal.Header>
-        <Modal.Body>
-          <Form >
-            <Form.Group className='mb-3' controlId='formBasicId'>
-              <Form.Label>Id</Form.Label>
-              <Form.Control
-                type='numeric'
-                value={id}
-                placeholder="Digite o id do cliente:"
-                onChange={(e)=>setId(e.target.value)}
-                required/>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='outline-dark' onClick={handleClose}>
-            Fechar
-          </Button>
-          <Button variant='outline-danger' onClick={handleDelete}>
-            Deletar
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    <Button 
+    variant='outline-danger'
+    className='float-end mx-1'
+    onClick={(e)=>handleDelete()}
+    >
+      Deletar
+    </Button>
     </div>
   );
 }
