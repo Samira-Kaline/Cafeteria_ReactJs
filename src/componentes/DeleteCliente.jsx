32eb845b8e418bd2a4d3ad4d5e7de615ad1575cf
Samira Kaline <samirakaline123@gmail.com>
2022-12-21 17:11:49 -0300

    Botão de Delete e update

--- src/componentes/DeleteCliente.jsx
+++ src/componentes/DeleteCliente.jsx
@@ -3,70 +3,7 @@ import {Button,Form,Modal} from 'react-bootstrap';
 import {TbTrash} from 'react-icons/tb';
 
 const DeleteClient = () =>{
-  const [id,setId] = useState(0);
-  const [show,setShow] = useState();;
-
-  const handleClose = () =>{
-    handleClean();
-    setShow(false);
-  }
-
-  const handleShow = () =>{
-    setShow(true);
-  }
-  const handleClean = () =>{
-    setId(0);
-  }
-
-  const handleDelete = () =>{
-    fetch(`http://localhost:3001/clientes/${id}`, { method: 'DELETE' })
-      .then((response) => {
-        return response.json();
-      })
-      .catch((error) => {
-        console.log('Resposta com insucesso!');
-      })
-      .finally();
-
-      handleClean();
-      handleClose();
-  }
-
-  return(
-    <div>
-      <Button 
-        variant='outline-danger '
-        className='float-end mx-1'
-        onClick={handleShow}
-      >
-        <TbTrash size={20}/>
-      </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>Deletar Cliente</Modal.Header>
-        <Modal.Body>
-          <Form >
-            <Form.Group className='mb-3' controlId='formBasicId'>
-              <Form.Label>Id</Form.Label>
-              <Form.Control
-                type='numeric'
-                value={id}
-                placeholder="Digite o id do cliente:"
-                onChange={(e)=>setId(e.target.value)}
-                required/>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='outline-dark' onClick={handleClose}>
-            Fechar
-          </Button>
-          <Button variant='outline-danger' onClick={handleDelete}>
-            Deletar
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
+  
 }
 
 export default DeleteClient;
